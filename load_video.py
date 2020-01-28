2c35c3c3b540bb491f58f9f58342855c1252b638
Yuxiang Gong <garygong0430@gmail.com>
2020-01-28 22:02:58 +0100

    delete

--- load_video.py
+++
@@ -1,9 +0,0 @@
-from auditok import split
-from auditok import AudioRegion
-audio_regions = split('./data/Track 2.wav')
-for region in audio_regions:
-    region.play(progress_bar=True)
-
-# region = AudioRegion.load('./data/Track 2.wav')
-#
-# region.split_and_plot()
